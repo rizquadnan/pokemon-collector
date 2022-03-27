1b8de414f3458fb2521bfd69bdbb3b2cc25182b5
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-03-28 06:02:56 +0700

    chore: move type check outside lint staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,11 +6,5 @@ const buildEslintCommand = (filenames) =>
     .join(" --file ")}`;
 
 module.exports = {
-  "*.{ts,tsx}": [
-    "yarn format",
-    buildEslintCommand,
-    // TODO: find way to use yarn type-check here to reuse scripts
-    // currently using script makes not respecting tsconfig.json
-    "tsc-files --pretty --noEmit",
-  ],
+  "*.{ts,tsx}": ["yarn format", buildEslintCommand],
 };
