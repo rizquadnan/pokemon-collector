a1c74bb40748428e8e7fbdb3484b491f14ce5620
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-04-01 15:53:19 +0700

    feat: implement navigation through menu

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled";
 import { Button } from "@sumup/circuit-ui";
+import Link from "next/link";
+import { ReactNode } from "react";
 import { HOME_MENU_TITLE, MY_POKEMON_MENU_TITLE } from "./constants";
 
 const MenuWrapper = styled.div({
@@ -15,15 +17,31 @@ type MenuProps = {
   onClickMyPokemon: () => void;
 };
 
+type CustomLinkProps = {
+  href: string;
+  children: ReactNode;
+};
+
+const CustomLink = (props: CustomLinkProps) => {
+  return (
+    <Link href={props.href} passHref>
+      <a css={{ textDecoration: "none" }}>{props.children}</a>
+    </Link>
+  );
+};
 const Menu = ({ onClickHome, onClickMyPokemon }: MenuProps) => {
   return (
     <MenuWrapper>
-      <Button variant="tertiary" onClick={onClickHome}>
-        {HOME_MENU_TITLE}
-      </Button>
-      <Button variant="tertiary" onClick={onClickMyPokemon}>
-        {MY_POKEMON_MENU_TITLE}
-      </Button>
+      <CustomLink href="/">
+        <Button variant="tertiary" onClick={onClickHome}>
+          {HOME_MENU_TITLE}
+        </Button>
+      </CustomLink>
+      <CustomLink href="/my-pokemon">
+        <Button variant="tertiary" onClick={onClickMyPokemon}>
+          {MY_POKEMON_MENU_TITLE}
+        </Button>
+      </CustomLink>
     </MenuWrapper>
   );
 };
