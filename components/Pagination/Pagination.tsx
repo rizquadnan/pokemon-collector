6fdf37572540e32991617626393002a71529d335
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-04-01 10:18:13 +0700

    fix: pagination change props

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -3,10 +3,27 @@ import {
   PaginationProps as PaginationProps_,
 } from "@sumup/circuit-ui";
 
-interface PaginationProps extends PaginationProps_ {}
+interface PaginationProps
+  extends Omit<
+    PaginationProps_,
+    "label" | "previousLabel" | "nextLabel" | "pageLabel"
+  > {
+  label?: string;
+  previousLabel?: string;
+  nextLabel?: string;
+  pageLabel?: (page: number) => string;
+}
 
-const Pagination = (props: PaginationProps): JSX.Element => {
-  return <>{Pagination_({ ...props })}</>;
+const Pagination = ({
+  label = "Pagination",
+  previousLabel = "Previous page",
+  nextLabel = "Next page",
+  pageLabel = (page) => `Go to page ${page}`,
+  ...props
+}: PaginationProps): JSX.Element => {
+  return (
+    <>{Pagination_({ ...props, label, previousLabel, nextLabel, pageLabel })}</>
+  );
 };
 
 export type { PaginationProps };
--- components/Pagination/requiredProps.ts
+++ components/Pagination/requiredProps.ts
@@ -1,10 +1,6 @@
 import { PaginationProps } from "./Pagination";
 
 export const requiredProps: PaginationProps = {
-  label: "Pagination",
   totalPages: 5,
-  previousLabel: "Previous page",
-  nextLabel: "Next page",
   onChange: (pageNum) => console.log(pageNum),
-  pageLabel: (page) => `Go to page ${page}`,
 };
